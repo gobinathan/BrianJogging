4d165a7324e1d2e9d7f55603d9009f8bd5251baf
Saravanan Devarajan <sara@mypowerit.net>
2011-10-18 16:42:11 +0530

    uaser module updated

--- sites/all/modules/brainjogging/js/bj_users.js
+++ sites/all/modules/brainjogging/js/bj_users.js
@@ -15,33 +15,34 @@ function toggle_parent_fields(){
 	$('#edit-learning-disablity-wrapper').children('label').children('span').remove();
 	
 	if($("#edit-roles option:selected").text() == 'teacher' || $("#edit-roles option:selected").text() == 'site administrator'){
-		$('#edit-teacher-wrapper').hide();
-		$('#edit-parent-fname-wrapper').hide();
-		$('#edit-parent-lname-wrapper').hide();
-		$('#edit-parent-mail-wrapper').hide();
-		$('#edit-reason-wrapper').hide();
-		$('#edit-grade-wrapper').hide();
-		$('#edit-learning-disablity-wrapper').hide();
+		$('#edit-profile-parent-fname-wrapper').hide();
+		$('#edit-profile-parent-lname-wrapper').hide();
+		$('#edit-profile-parent-email-wrapper').hide();
+		$('#edit-profile-reason-wrapper').parent('fieldset').hide();
+		//$('#edit-teacher-wrapper').hide();
+		//$('#edit-profile-reason-wrapper').hide();
+		//$('#edit-grade-wrapper').hide();
+		//$('#edit-learning-disablity-wrapper').hide();
 	}
 	else{
-		$('#edit-teacher-wrapper').show();
+		//$('#edit-teacher-wrapper').show();
 		$('#edit-teacher-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
 		
-		$('#edit-parent-fname-wrapper').show();
-		$('#edit-parent-fname-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
+		$('#edit-profile-parent-fname-wrapper').show();
+		$('#edit-profile-parent-fname-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
 		
-		$('#edit-parent-lname-wrapper').show();
-		$('#edit-parent-lname-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
+		$('#edit-profile-parent-lname-wrapper').show();
+		$('#edit-profile-parent-lname-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
 		
-		$('#edit-parent-mail-wrapper').show();
-		$('#edit-parent-mail-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
+		$('#edit-profile-parent-email-wrapper').show();
+		$('#edit-profile-parent-email-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
 		
-		$('#edit-reason-wrapper').show();
+		//$('#edit-profile-reason-wrapper').show();
 		
-		$('#edit-grade-wrapper').show();
+		//$('#edit-grade-wrapper').show();
 		$('#edit-grade-wrappe').children('label').append('<span title="This field is required." class="form-required">*</span>');
 		
-		$('#edit-learning-disablity-wrapper').show();
+		//$('#edit-learning-disablity-wrapper').show();
 		$('#edit-learning-disablity-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
 	}
 }
