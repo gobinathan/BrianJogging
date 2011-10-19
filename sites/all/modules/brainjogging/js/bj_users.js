823b012d7937a5980b716769eb6cd8f0df6ba290
Saravanan Devarajan <sara@mypowerit.net>
2011-10-19 12:37:16 +0530

    user module updated

--- sites/all/modules/brainjogging/js/bj_users.js
+++ sites/all/modules/brainjogging/js/bj_users.js
@@ -7,25 +7,30 @@ $(document).ready(function() {
 
 function toggle_parent_fields(){
 	$('#edit-teacher-wrapper').children('label').children('span').remove();
-	$('#edit-parent-fname-wrapper').children('label').children('span').remove();
-	$('#edit-parent-lname-wrapper').children('label').children('span').remove();
-	$('#edit-parent-mail-wrapper').children('label').children('span').remove();
-	$('#edit-parent-mail-wrapper').children('label').children('span').remove();
-	$('#dit-grade-wrapper').children('label').children('span').remove();
+	$('#edit-profile-parent-fname-wrapper').children('label').children('span').remove();
+	$('#edit-profile-parent-lname-wrapper').children('label').children('span').remove();
+	$('#edit-profile-parent-email-wrapper').children('label').children('span').remove();
+	$('#edit-grade-wrapper').children('label').children('span').remove();
 	$('#edit-learning-disablity-wrapper').children('label').children('span').remove();
 	
-	if($("#edit-roles option:selected").text() == 'teacher' || $("#edit-roles option:selected").text() == 'site administrator'){
+	if($("#edit-roles option:selected").text() == 'teacher' || $("#edit-roles option:selected").text() == 'school administrator'){
+		$('#edit-profile-parent-fname-wrapper').hide();
+		$('#edit-profile-parent-lname-wrapper').hide();
+		$('#edit-profile-parent-email-wrapper').hide();
+		//$('#edit-profile-reason-wrapper').parent('fieldset').hide();
+		$('#edit-teacher-wrapper').hide();
+		$('#edit-profile-reason-wrapper').hide();
+		$('#edit-grade-wrapper').hide();
+		$('#edit-learning-disablity-wrapper').hide();
+	}
+	else if($("#edit-roles option:selected").text() == 'site administrator'){
 		$('#edit-profile-parent-fname-wrapper').hide();
 		$('#edit-profile-parent-lname-wrapper').hide();
 		$('#edit-profile-parent-email-wrapper').hide();
 		$('#edit-profile-reason-wrapper').parent('fieldset').hide();
-		//$('#edit-teacher-wrapper').hide();
-		//$('#edit-profile-reason-wrapper').hide();
-		//$('#edit-grade-wrapper').hide();
-		//$('#edit-learning-disablity-wrapper').hide();
 	}
 	else{
-		//$('#edit-teacher-wrapper').show();
+		$('#edit-teacher-wrapper').show();
 		$('#edit-teacher-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
 		
 		$('#edit-profile-parent-fname-wrapper').show();
@@ -37,12 +42,12 @@ function toggle_parent_fields(){
 		$('#edit-profile-parent-email-wrapper').show();
 		$('#edit-profile-parent-email-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
 		
-		//$('#edit-profile-reason-wrapper').show();
+		$('#edit-profile-reason-wrapper').show();
 		
-		//$('#edit-grade-wrapper').show();
-		$('#edit-grade-wrappe').children('label').append('<span title="This field is required." class="form-required">*</span>');
+		$('#edit-grade-wrapper').show();
+		$('#edit-grade-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
 		
-		//$('#edit-learning-disablity-wrapper').show();
+		$('#edit-learning-disablity-wrapper').show();
 		$('#edit-learning-disablity-wrapper').children('label').append('<span title="This field is required." class="form-required">*</span>');
 	}
 }
